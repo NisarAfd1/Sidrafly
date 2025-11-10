b375182174b65bad724a80722221dbade5656298
Nisarafd1 <graduatedgamer0300@gmail.com>
2025-11-10 21:34:32 +0500

    Initial commit: SidraFly base pages)

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+// --- SidraFly Firebase configuration (Compat version) ---
+const firebaseConfig = {
+  apiKey: "AIzaSyDgOAWWOXKbCKwpGaIZOqkZmHIA6z9wJ8M",
+  authDomain: "sidrafly-57bed.firebaseapp.com",
+  projectId: "sidrafly-57bed",
+  storageBucket: "sidrafly-57bed.appspot.com",
+  messagingSenderId: "441230761869",
+  appId: "1:441230761869:web:f054dc8c8cfd29e8c4a7c4"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
